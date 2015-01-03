720fdc8e95ffcd6cc8fc36cdb4393673cb3eebb9
krishnabatham <krishnabatham@users.noreply.github.com>
2015-01-03 17:51:33 +0530

    Update Global.asax.cs
    test2

--- MvcApplication1/Global.asax.cs
+++ MvcApplication1/Global.asax.cs
@@ -15,7 +15,7 @@ namespace MvcApplication1
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-            //Testtestest
+            //TesttestestTEst22
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
