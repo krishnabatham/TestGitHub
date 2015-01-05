32be2cb8dc8d5c886fb62e435d44b9971ce3cff0
krishnabatham <krishnabatham@gmail.com>
2015-01-05 11:42:58 +0530

    test commit

--- MvcApplication1/App_Start/WebApiConfig.cs
+++ MvcApplication1/App_Start/WebApiConfig.cs
@@ -10,7 +10,7 @@ namespace MvcApplication1
         public static void Register(HttpConfiguration config)
         {
             config.Routes.MapHttpRoute(
-                name: "DefaultApi",
+                name: "DefaultApi"  ,
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
